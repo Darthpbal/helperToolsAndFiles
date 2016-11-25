2034d0fc6828069c91db26595f24219c9792daef
Darthpbal <Ronquillo.paul@gmail.com>
2016-11-25 13:52:55 -0700

    progressed through the lesson

--- languages/python/codecademy/intro.py
+++ languages/python/codecademy/intro.py
@@ -439,3 +439,80 @@ for key in prices:
     print "price: %s" % prices[key]
     print "stock: %s\n" % stock[key]
 s
+
+
+
+
+
+
+
+
+
+
+
+
+# lists and ranges and stuff
+
+def my_function(x):
+    for i in range(0, len(x)):
+        x[i] = x[i] * 2
+    return x
+
+print my_function(range(3)) # Add your range between the parentheses!
+# range(stop)
+# range(start, stop)
+# range(start, stop, step)
+
+
+
+
+
+
+
+
+n = [[1, 2, 3], [4, 5, 6, 7, 8, 9]]
+# Add your function here
+def flatten(lists):
+    results = []
+    for numbers in lists:
+        for number in numbers:
+            results.append(number)
+    return results
+print flatten(n)
+
+
+
+
+
+
+
+
+
+from random import randint
+
+board = []
+
+for x in range(0, 5):
+    board.append(["O"] * 5)
+
+def print_board(board):
+    for row in board:
+        print " ".join(row)
+
+print_board(board)
+
+def random_row(board):
+    return randint(0, len(board) - 1)
+
+def random_col(board):
+    return randint(0, len(board[0]) - 1)
+
+ship_row = random_row(board)
+ship_col = random_col(board)
+guess_row = int(raw_input("Guess Row:"))
+guess_col = int(raw_input("Guess Col:"))
+
+print ship_row
+print ship_col
+
+# Write your code below!
