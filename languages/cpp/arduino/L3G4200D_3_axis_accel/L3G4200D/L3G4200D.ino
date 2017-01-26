bc7a5959f4aab684940cfcfc13a5baa735a0457a
darthpbal <Ronquillo.paul@gmail.com>
2017-01-26 04:07:33 -0700

    added gyro files

---
+++ languages/cpp/arduino/L3G4200D_3_axis_accel/L3G4200D/L3G4200D.ino
@@ -0,0 +1,11 @@
+/*
+L3G4200D
+Power Requirement: 2.7 to 6.5 VDC
+Current draw: 6.7mA @ 3.3VDC, 7mA @ 5VDC
+Communication: I2C (up to 400kHz) or SPI (10MHz; 4 & 3 wire)
+Dimensions: 0.85 x 0.80 in (2.16 x 2.03cm)
+Operating temp range: -40 to +185 °F (-40 to +85 °C)
+
+library that might be usable for this gyro
+https://github.com/pololu/l3g-arduino
+*/
