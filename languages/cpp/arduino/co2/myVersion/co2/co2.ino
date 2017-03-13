daa77bc364d16d04efbe55ed00284b199eacaa32
darthpbal <Ronquillo.paul@gmail.com>
2017-03-13 00:53:10 -0700

    added co2 library files

---
+++ languages/cpp/arduino/co2/myVersion/co2/co2.ino
@@ -0,0 +1,14 @@
+/*
+main link to board product page.
+http://sandboxelectronics.com/?product=mh-z16-ndir-co2-sensor-with-i2cuart-5v3-3v-interface-for-arduinoraspeberry-pi
+*/
+
+void setup() {
+    Serial.begin(9600);
+}
+
+
+
+void loop() {
+
+}
